ec225be3f543d0b48d92a3a7a1b0820e792cf1dd
Michael Patterson <mjpatter88@gmail.com>
2014-10-09 22:52:54 -0400

    Added functionality to add the data to the db and retireve. Now I just need to add the functionality to process the data and display the results.

--- pyPhone.py
+++ pyPhone.py
@@ -1,4 +1,7 @@
+# Written for Python 2.7.5
 import sqlite3
+
+fileNames = ['data/Apr5thruMay4.txt', 'data/May5thruJune4.txt', 'data/June5thruJuly4.txt']
 
 def tableExists(db):
     cursor = db.cursor()
@@ -16,14 +19,48 @@ def tableExists(db):
 def createTable(db):
     cursor = db.cursor()
     cursor.execute('''
-        CREATE TABLE calls(id INTEGER PRIMARY KEY, phone_number TEXT, duration INTEGER, incoming INTEGER)
+        CREATE TABLE calls(id INTEGER PRIMARY KEY, phone_number TEXT, duration INTEGER,
+                            incoming INTEGER, date TEXT, time TEXT)
     ''')
+    db.commit()
     return
 
 def populateTable(db):
+    # For each line in each file, add to the database
+    for fileName in fileNames:
+        file = open(fileName, 'r')
+        file.readline()                 #ignore the first line which is just the header
+        for line in file:
+            parts = line.split("\t")
+            date = parts[0]
+            time = parts[1]
+            phoneNumber = parts[2]
+            duration = parts[7]
+            if parts[6] == "Incoming CL":
+                incoming = 1
+            else:
+                incoming = 0
+
+            cursor = db.cursor()
+            cursor.execute('''
+                INSERT INTO calls(phone_number, duration, incoming, date, time)
+                VALUES(?,?,?,?,?)''', (phoneNumber, duration, incoming, date, time))
+            db.commit()
+        file.close()
+
     return
 
 def analyzeData(db):
+    # Total calls, incoming, outgoing (and percents). top 5 most frequent with percentages, etc.
+    cursor = db.cursor()
+    cursor.execute('''SELECT phone_number, duration, incoming, date, time FROM calls''')
+    for call in cursor:
+        print call[0], call[1], "minutes,",
+        if call[2] == 1:
+            print "incoming,",
+        else:
+            print "outgoing,",
+        print call[3], call[4]
     return
 
 
